67e786436599b97d335df72aeb9053dbe6ac5c78
Andres Colonia <andreskal@gmail.com>
2020-04-12 21:34:19 -0400

    feat: support to views
    separate controller and include support to views

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require 'rulers/version'
 require 'rulers/routing'
 require 'rulers/utils'
 require 'rulers/dependencies'
+require 'rulers/controller'
 
 module Rulers
   class Application
@@ -14,16 +15,6 @@ module Rulers
       controller = klass.new(env)
       text = controller.send(act)
       [200, { 'Content-Type' => 'text/html' }, [text]]
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
